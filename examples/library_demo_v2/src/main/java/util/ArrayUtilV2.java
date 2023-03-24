eef9ed993f6fb402603d805b4569ae911c484423
huzihan <hzh1941857127@163.com>
2023-03-25 00:55:17 +0800

    chore: add demo library projects

---
+++ examples/library_demo_v2/src/main/java/util/ArrayUtilV2.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.util.Arrays;
+
+public class ArrayUtilV2 {
+    public static void sort(int[] nums, boolean desc) {
+        Arrays.sort(nums);
+        if (desc) {
+            for (int idx = 0; idx < nums.length / 2; idx++) {
+                int tmp = nums[idx];
+                nums[idx] = nums[nums.length - 1 - idx];
+                nums[nums.length - 1 - idx] = tmp;
+            }
+        }
+    }
+
+    public static void sort(boolean desc, int[] nums) throws IllegalArgumentException {
+        if (nums == null) {
+            throw new IllegalArgumentException("illegal array to sort, nums array can not be null");
+        }
+
+        Arrays.sort(nums);
+        if (desc) {
+            for (int idx = 0; idx < nums.length / 2; idx++) {
+                int tmp = nums[idx];
+                nums[idx] = nums[nums.length - 1 - idx];
+                nums[nums.length - 1 - idx] = tmp;
+            }
+        }
+    }
+}
---
+++ examples/library_demo_v2/src/main/java/util/MathUtilV2.java
@@ -0,0 +1,7 @@
+package util;
+
+public class MathUtilV2 {
+    public static int sumv2(int a, int b, int c) {
+        return a + b + c;
+    }
+}
